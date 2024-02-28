d7d2f672f14ab27d8bf164db235fc3dc2ac48a74
KunjHedapara <120015557+KunjHedapara@users.noreply.github.com>
2024-02-29 10:45:14 +1200

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,21 +7,21 @@ export class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">Daily Updates</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+                    <div class="container-fluid">
+                        <Link class="navbar-brand" to="/">Daily Updates</Link>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/home">Home</Link></li>
-                                <li className="nav-link active nav-item"><Link className="nav-item" to="/business" style={{ textDecoration: 'none', color: "#fff" }}>Business</Link></li>
-                                <li className="nav-link active"><Link className="nav-item" to="/entertainment" style={{ textDecoration: 'none', color: "#fff" }}>Entertainment</Link></li>
-                                <li className="nav-link active"><Link className="nav-item" to="/health" style={{ textDecoration: 'none', color: "#fff" }}>Health</Link></li>
-                                <li className="nav-link active"><Link className="nav-item" to="/science" style={{ textDecoration: 'none', color: "#fff" }}>Science</Link></li>
-                                <li className="nav-link active"><Link className="nav-item" to="/sports" style={{ textDecoration: 'none', color: "#fff" }}>Sports</Link></li>
-                                <li className="nav-link active"><Link className="nav-item" to="/technology" style={{ textDecoration: 'none', color: "#fff" }}>Technology</Link></li>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/home">Home</Link></li>
+                                <li class="nav-link active nav-item"><Link class="nav-item" to="/business" style={{ textDecoration: 'none', color: "#fff" }}>Business</Link></li>
+                                <li class="nav-link active"><Link class="nav-item" to="/entertainment" style={{ textDecoration: 'none', color: "#fff" }}>Entertainment</Link></li>
+                                <li class="nav-link active"><Link class="nav-item" to="/health" style={{ textDecoration: 'none', color: "#fff" }}>Health</Link></li>
+                                <li class="nav-link active"><Link class="nav-item" to="/science" style={{ textDecoration: 'none', color: "#fff" }}>Science</Link></li>
+                                <li class="nav-link active"><Link class="nav-item" to="/sports" style={{ textDecoration: 'none', color: "#fff" }}>Sports</Link></li>
+                                <li class="nav-link active"><Link class="nav-item" to="/technology" style={{ textDecoration: 'none', color: "#fff" }}>Technology</Link></li>
                             </ul>
                         </div>
                     </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,14 +5,14 @@ export default class NewsItem extends Component {
         let { title, description, ImageUrl, newsUrl, author, date, source } = this.props
         return (
             <div>
-                <div className="card my-3">
+                <div class="card my-3">
                     <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '92%', zIndex: '1' }}>{source}</span>
-                    <img src={!ImageUrl ? "https://static.toiimg.com/thumb/msid-107836901,width-1070,height-580,imgsize-54772,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" : ImageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unkown" : author} on {new Date(date).toGMTString()} </small></p>
-                        <a href={newsUrl} className="btn btn-sm btn-dark">Read more</a>
+                    <img src={!ImageUrl ? "https://static.toiimg.com/thumb/msid-107836901,width-1070,height-580,imgsize-54772,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" : ImageUrl} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{title}...</h5>
+                        <p class="card-text">{description}...</p>
+                        <p class="card-text"><small class="text-muted">By {!author ? "Unkown" : author} on {new Date(date).toGMTString()} </small></p>
+                        <a href={newsUrl} class="btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
             </div>
